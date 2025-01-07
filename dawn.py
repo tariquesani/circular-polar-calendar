6ca09ceda9c6c13903932c767a95d3d4ff368ab0
Tarique Sani <tariquesani@gmail.com>
2025-01-07 13:51:17 +0530

    Added config for offset of rings and footers

--- dawn.py
+++ dawn.py
@@ -7,6 +7,7 @@ from components.base_calendar_plotter import BaseCalendarPlotter
 
 from components.layer_dawn import DawnLayer
 from components.layer_temperature import TemperatureLayer
+from components.layer_precipitation import PrecipitationLayer
 
 
 def main():
@@ -22,12 +23,17 @@ def main():
 
         dawn_data, weather_data, city_data = data_handler.load_data()
         config.city_data = city_data
+        config.temp_offset = 0.062 # 8.2% offset for temperature ring, higher means more inside the circle
+        config.precip_offset = 0.042 # 4.2% offset for precipitation ring
+        config.temp_footer_offset = 0.01 # 1% offset for temperature footer
+        config.precip_footer_offset = 0.04 # 4% offset for precipitation footer
 
         dawn_layer = DawnLayer(dawn_data, config)
         temperature_layer = TemperatureLayer(weather_data, config)
+        precipitation_layer = PrecipitationLayer(weather_data, config)
 
         plotter = BaseCalendarPlotter(config)
-        plotter.create_plot(layers=[dawn_layer, temperature_layer])
+        plotter.create_plot(layers=[dawn_layer, temperature_layer, precipitation_layer])
 
     except ConfigurationError as e:
         print(f"Configuration error: {str(e)}")
